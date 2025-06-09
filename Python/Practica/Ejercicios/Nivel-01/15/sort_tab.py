58d23ca69d64fd1ce581aad9845c89b565943794
ChechiDev <chechidatadev@gmail.com>
2025-06-09 17:03:27 +0200

    ADDED: Ex-15-sort_tab.py

---
+++ Python/Practica/Ejercicios/Nivel-01/15/sort_tab.py
@@ -0,0 +1,42 @@
+import sys
+import os
+
+"""
+Escribe un programa que ordene un array de int en orden ascendente.
+El prototipo del main deberá ser el siguiente:
+
+def main():
+
+El prototipo de la función deberá ser el siguiente:
+
+def sort_tab(arr);
+
+"""
+def sort_tab(arr):
+    """
+    Ordena un array de enteros en orden ascendente.
+    Return:
+        list: Lista de enteros ordenada.
+    """
+    return sorted(arr)
+
+
+def main():
+    if len(sys.argv) == 2:
+
+        try:
+            # Comprobamos con un parámetro de compresión que lo que se introduce son enteros: Ej: 3,2,1
+            arr = [int(x) for x in sys.argv[1].split(",")]
+            print(sort_tab(arr))
+
+
+        except ValueError:
+            print("Error: El argumento debe ser una lista de enteros.")
+            return
+
+    else:
+        print("Uso: python sort_tab.py <list>")
+
+
+if __name__ == "__main__":
+    main()
