2d937114d1545b284c19189e8145de987949c56d
ChechiDev <chechidatadev@gmail.com>
2025-06-09 22:27:10 +0200

    ADD: str_inv_case.py

--- Python/Practica/Ejercicios/Nivel-02/ex25/mov_13.py
+++ Python/Practica/Ejercicios/Nivel-02/ex25/mov_13.py
@@ -3,33 +3,33 @@ import os
 import string
 
 """
-Escribe un programa que tome un string y lo muestre, moviendo cada letra 1 posición en el alfabeto.
-"z" será "a" y "Z" será "A".
+Escribe un programa que tome un string y lo muestre, moviendo cada letra 13 posiciones en el alfabeto.
+"z" será "m" y "Z" será "M".
 Las cajas no se verán afectadas.
 La string de salida estará seguida de una nueva línea.
 Si el número de argumentos no es 1, simplemente se mostrará una nueva línea.
 Ejemplos:
-$>python mov_alpha.py “abc”
-bcd
+$>python mov_13 “abc”
+nop
 
-$>python mov_alpha.py “Les stagiaires du staff ne sentent pas toujours tres bon.” | cat -e
-Mft tubhjbjsft ev tubgg of tfoufou qbt upvkpvst usft cpo.$
+$>python mov_13 “My horse is Amazing.” | cat -e
+Zl ubefr vf Nznmvat.$
 
-$>python mov_alpha.py “AkjhZ zLKIJz , 23y “ | cat -e
-BlkiA aMLJKa , 23z $
+$>python mov_13 “AkjhZ zLKIJz , 23y “ | cat -e
+NxwuM mYXVWm , 23l $
 
-$>python mov_alpha.py | cat -e
+$>python mov_13 | cat -e
 $
 $>
 
-$>python mov_alpha.py “” | cat -e
+$>python mov_13 “” | cat -e
 $
 $>
 """
 
 def get_next_char(char):
     """
-    Devuelve la siguiente letra en el alfabeto.
+    Devuelve la siguiente letra en el alfabeto desplazando 13 posiciones.
     Cuando llega la final del alfabeto
     Si no es letra devuelve le mismo carácter
     """
@@ -40,20 +40,20 @@ def get_next_char(char):
         # Convertimos carácter a int
         idx = alph_low.index(char)
         # Calculamos el modulo, para el índice
-        return alph_low[(idx + 1) % 26]
+        return alph_low[(idx + 13) % 26]
 
     # Lógica para las mayúsculas
     elif char in alph_up:
         idx = alph_up.index(char)
-        return alph_up[(idx + 1) % 26]
+        return alph_up[(idx + 13) % 26]
 
     else:
         return char
 
 
-def mov_alpha(text):
+def mov_13(text):
     """
-    Devuelve un nuevo string donde cada letra se desplaza una posición en el alfabeto.
+    Devuelve un nuevo string donde cada letra se desplaza 13 posiciones en el alfabeto.
     La función respeta mayúsculas y minúsculas.
     """
     os.system("cls")
@@ -68,7 +68,7 @@ def mov_alpha(text):
 def main():
     if len(sys.argv) == 2:
         text = sys.argv[1]
-        print(mov_alpha(text))
+        print(mov_13(text))
 
     else:
         print()
