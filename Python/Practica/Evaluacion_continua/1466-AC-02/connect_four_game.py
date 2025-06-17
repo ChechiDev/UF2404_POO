4e6e786fe9fcf1768cb9535cb1a336b705e59eb0
Sergi Perez <chechidatadev@gmail.com>
2025-06-17 11:22:40 +0200

    ADD: AC-CONT -> First commit connect_four_game.py

---
+++ Python/Practica/Evaluacion_continua/1466-AC-02/connect_four_game.py
@@ -0,0 +1,50 @@
+
+
+"""
+# Ejercicio
+
+Desarrollar el juego completo de **Conecta Cuatro** en la terminal, en el que un jugador humano compite contra una **CPU con inteligencia variable** (`easy`, `normal`, `hard`).
+El juego debe ser completamente funcional, visual y ejecutable desde consola.
+"""
+"""
+## Objetivos
+
+1. Practicar el uso de listas bidimensionales `board`
+2. Control de flujo `if`, `for`, `while`
+3. Funciones puras y control del estado
+4. Desarrollo incremental y Q&A (”testeo”) de funciones
+5. Pensamiento algorítmico: detección de victoria y lógica de CPU
+"""
+"""
+## Hints
+
+- Usa `board[:]` y listas por comprensión para copiar el tablero sin errores
+- La sentencia `while True` permite ejecutar un conjunto de sentencias de código en Python hasta que se rompa la ejecución utilizando algún mecanismo como la palabra reservada `break`
+- Añade comentarios en tu código
+- Haz pruebas de cada parte por separado antes de combinar todo
+"""
+"""
+## Extras
+
+- Realizar una introducción al juego (animación)
+- Permitir al jugador escoger colores de ficha (ANSI)
+- Modo dos jugadores humanos
+- Añadir dificultad `"insane"` con lógica más avanzada
+- Crear un menú de elección
+- Historial de partidas
+"""
+
+
+
+
+
+
+def game():
+    """
+    Lógica principal del juego
+    """
+    pass
+
+
+if __name__ == "__main__":
+    game()
