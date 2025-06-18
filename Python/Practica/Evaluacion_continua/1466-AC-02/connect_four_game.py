84319d8dd9162767aa54421e046985e0e40bf062
ChechiDev <chechidatadev@gmail.com>
2025-06-18 16:20:43 +0200

    UPDT: create_board function + print_board function added

--- Python/Practica/Evaluacion_continua/1466-AC-02/connect_four_game.py
+++ Python/Practica/Evaluacion_continua/1466-AC-02/connect_four_game.py
@@ -37,6 +37,16 @@ El juego debe ser completamente funcional, visual y ejecutable desde consola.
 - Historial de partidas
 """
 
+"""
+Part 1: Crear el tauler »Crear una funció `create_board()` que retorni una matriu 6x7 de `.` »Crear una funció `print_board()` que el mostri per pantalla
+Part 2: Inserir fitxes »Implementar la funció `insert_piece(board, column, piece)` »La fitxa ha de caure fins a la posició lliure més baixa
+Part 3: Torns i validacions »Alternar els torns entre el jugador `X` i la CPU `0` »Verificar si la columna és vàlida abans de jugar
+Part 4: Detecció de victòria »Crear una funció `check_win(board, piece)` »Ha de detectar 4 en ratlla en: »  Horitzontal »  Vertical »  Diagonal descendent (`/`) »  Diagonal ascendent (`\`)
+Part 5: Intel·ligència de la CPU »Crear diferents funcions per a la CPU: »  `cpu_easy`: tria una columna aleatòria »  `cpu_normal`: intenta guanyar o bloquejar el jugador »  `cpu_hard`: utilitza una heurística per triar la millor jugada
+Part 6: Flux del joc »Mostrar el tauler després de cada torn »Mostrar missatges de victòria o empat »Permetre triar la dificultat al començar el joc
+"""
+
+
 # Variabless globales para configuración del juego:
 ROWS = 6
 COLUMNS = 7
@@ -53,25 +63,39 @@ def clear_terminal():
         os.system("clear")
 
 
-def create_board(x, y):
-    matrix = []
+def create_board(x: int, y: int) -> list:
+    """
+    Creates a board game grid\n
+    Arg1 x: int: ROWS \n
+    Arg2 y: int: COLUMNS\n
+    Return: List: 2D array with the board generated
+    """
+    board = []
 
+    # Generamos los números del header
+    print("  ".join(str(n) for n in range(y)))
+
+    # Generamos el board de juego.
     for i in range(x):
         row = []
         for j in range(y):
-            if (i == 0):
-                row.append(str(j))
+            row.append(". ")
 
-            else:
-                row.append(".")
+        board.append(row)
 
-        matrix.append(row)
+    # print(type(board))
+    return board
 
-    # Comprobamos matriz
-    print(type(matrix))
 
-    # Mostramos contenido de la lista 2D:
-    for row in matrix:
+def print_board(board: list) -> list:
+    """
+    Muestra el tablero por pantalla\n
+    Arg: 2D List\n
+    Return: List: 2D array rendered in CMD.
+    """
+
+    # print(type(list))
+    for row in board:
         print(" ".join(row))
 
 
@@ -79,8 +103,12 @@ def game():
     """
     Lógica principal del juego
     """
+    # Limpiamos terminal:
     clear_terminal()
+
     board = create_board(ROWS, COLUMNS)
+    print_board(board)
+
 
 if __name__ == "__main__":
     game()
