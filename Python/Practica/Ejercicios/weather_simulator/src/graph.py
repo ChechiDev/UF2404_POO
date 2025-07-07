1139cf2d0a254785ff07a2127ff9a44e48d30059
ChechiDev <chechidatadev@gmail.com>
2025-07-07 15:54:03 +0200

    UPDT: graph.py => Testing get_data to plot

--- Python/Practica/Ejercicios/weather_simulator/src/graph.py
+++ Python/Practica/Ejercicios/weather_simulator/src/graph.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 from .utils.json_builder import JsonBuilder
 from .weather import WeatherStation
 import os
+import pprint
+import json
 
 class GraphGenerator:
     def __init__(self, days: int = 0):
@@ -14,24 +16,43 @@ class GraphGenerator:
 
     def get_data(self, cities_name = None, days = None):
 
+        if days is None:
+            days = self.w_st._max_days
+
         plot_result = {}
 
         for city in self.city_names:
-            # print(self.data[city])
-            for day, temp in self.data[city].items():
-                print(f"{city}: min_temp = {temp['min_temp']}")
+            city_key = self.data.get(city)
+            # print(city_key)
+            min_temp = []
+            med_temp = []
+            max_temp = []
 
+            for i in range(1, days + 1):
+                data_day = city_key.get(f"Day-{i}")
+                # print(data_day)
+
+                # si hay datos...
+                if data_day:
+                    min_temp.append(data_day.get("min_temp", 0))
+                    med_temp.append(data_day.get("med_temp", 0))
+                    max_temp.append(data_day.get("max_temp", 0))
+
+            plot_result[city] = [min_temp, med_temp, max_temp]
+            # print(json.dumps(plot_result, indent=4))
+
+        return plot_result
 
 
     def plot_temp(self):
-        pass
+        data = self.get_data()
+        print(data)
 
 
 if __name__ == "__main__":
-    import pprint
     os.system("cls")
     # graph = GraphGenerator("Barcelona", days=30)
     # graph.plot_temp()
     g = GraphGenerator()
-    print(g.get_data())
+    g.plot_temp()
     # pprint.pprint(g.data)
